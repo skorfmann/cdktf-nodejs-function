194aa599977f82aeb94fb82a7673a967aee638ab
Sebastian Korfmann <sebastian@korfmann.net>
2021-06-01 22:24:35 +0200

    Initial commit generated by projen

---
+++ .projenrc.js
@@ -0,0 +1,17 @@
+const { ConstructLibraryCdktf } = require('projen');
+const project = new ConstructLibraryCdktf({
+  author: 'Sebastian Korfmann',
+  authorAddress: 'sebastian@korfmann.net',
+  cdktfVersion: '0.4.0',
+  defaultReleaseBranch: 'main',
+  name: 'cdktf-nodejs-function',
+  repositoryUrl: 'https://github.com/sebastian/cdktf-nodejs-function.git',
+
+  // deps: [],                          /* Runtime dependencies of this module. */
+  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],                       /* Build dependencies for this module. */
+  // packageName: undefined,            /* The "name" in package.json. */
+  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  // releaseWorkflow: undefined,        /* Define a GitHub workflow for releasing from "main" when new versions are bumped. */
+});
+project.synth();
